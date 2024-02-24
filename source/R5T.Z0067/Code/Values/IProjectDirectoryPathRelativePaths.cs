d19de8ae49b9e15f2270527c382da3d293c0c2ab
David Coats <david@getsafetycone.com>
2024-02-24 00:57:42 -0800

    Add project-relative file path values.

--- source/R5T.Z0067/Code/Values/IProjectDirectoryPathRelativePaths.cs
+++ source/R5T.Z0067/Code/Values/IProjectDirectoryPathRelativePaths.cs
@@ -13,8 +13,23 @@ namespace R5T.Z0067
         Z001.IProjectDirectoryPathRelativePaths
     {
         /// <summary>
+        /// <para><value>Code\Documentation.cs</value></para>
+        /// </summary>
+        public string Documentation_cs => @"Code\Documentation.cs";
+
+        /// <summary>
+        /// <para><value>Code\Instances.cs</value></para>
+        /// </summary>
+        public string Instances_cs => @"Code\Instances.cs";
+
+        /// <summary>
         /// <para><value>Code\Program.cs</value></para>
         /// </summary>
         public string Program_cs => @"Code\Program.cs";
+
+        /// <summary>
+        /// <para><value>Project Plan.md</value></para>
+        /// </summary>
+        public string Project_Plan_md => @"Project Plan.md";
     }
 }
